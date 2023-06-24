30a9356e262923ecb895e4349800bb5921a805dc
Sean Park <seanpark132@gmail.com>
2023-06-24 04:55:46 -0700

    bug fixes for 1.0 deployment

--- src/app/components/Game/M/Score.tsx
+++ src/app/components/Game/M/Score.tsx
@@ -35,7 +35,7 @@ export default function Score(props: Props) {
             <div className="flex flex-col items-center text-center">
                 <em><p className="text-6xl text-green-400 font-extrabold">Correct!</p></em>
                 <h2 className="font-normal mt-6 break-normal">{props.selectedResponse}</h2>
-                <h2 className="my-4 text-green-400">was a Human's response!</h2>
+                <h2 className="my-4 text-green-400">was a Human&apos;s response!</h2>
                 <h2>Current score: {props.score}</h2>
                 {didUserSubmit ? <h1 className='mt-8 py-2 px-4 self-center text-center'>Waiting for others...</h1>
                 :<button className="btn-submit" onClick={() => handleNextRound()} >Next Round</button>}                         
@@ -48,7 +48,7 @@ export default function Score(props: Props) {
             <div className="flex flex-col items-center text-center">
                 <em><p className="text-6xl text-red-500 font-extrabold">Incorrect!</p></em>
                 <h2 className="font-normal mt-6 break-normal">{props.selectedResponse}</h2>
-                <h2 className="my-4 text-red-500">was a Robot's response!</h2>
+                <h2 className="my-4 text-red-500">was a Robot&apos;s response!</h2>
                 <h2>Current score: {props.score}</h2>
                 {didUserSubmit ? <h1 className='mt-8 py-2 px-4 self-center text-center'>Waiting for others...</h1>
                 :<button className="btn-submit" onClick={() => handleNextRound()} >Next Round</button>}                      
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
     return (
         <nav>      
             <div className="flex items-center justify-center w-screen p-6">
                 <Link href="" className="">
-                    <img className=" max-h-8" src="logo.webp"></img>
+                    <Image className="max-h-8" src="logo.webp" alt="logo"></Image>
                 </Link>
             </div>
         </nav>
