81cf8be78996db420b0215b61743fca643897225
Sean Park <seanpark132@gmail.com>
2023-06-28 03:09:07 -0700

    disabled input response changes after submitting

--- src/app/components/Game/M/Write.tsx
+++ src/app/components/Game/M/Write.tsx
@@ -37,7 +37,11 @@ export default function Write(props: Props) {
                 className="input-human-response" 
                 id="humanResponse"
                 value={inputUserResponse}
-                onChange={(e) => setInputUserResponse(e.target.value)}
+                onChange={(e) => {
+                    if (!didUserSubmit) {
+                        setInputUserResponse(e.target.value);
+                    };
+                }}
             />  
             {/* <div className="mt-1 w-full h-5"><p className="text-sm float-right mr-1">Word Count: 0/30</p></div> */}
             {didUserSubmit? <h1 className='mt-8 py-2 px-4 self-center text-center'>Waiting for others...</h1>
