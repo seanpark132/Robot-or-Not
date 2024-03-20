fd8bc65911f88870f10550c59fc5cb7daa4c8115
Sean Park <125317910+seanpark132@users.noreply.github.com>
2024-03-19 20:52:13 -0700

    Dev/planetscale migration (#9)
    * updated schema.prisma to postgresql db (neon)
    
    * updated openAI gpt version to gpt-4

--- src/app/api/generateQuestions/route.ts
+++ src/app/api/generateQuestions/route.ts
@@ -1,58 +1,58 @@
 import OpenAI from "openai";
 import { NextResponse } from "next/server";
-import { prisma } from "@root/lib/prismaClient"
+import { prisma } from "@root/lib/prismaClient";
 
-const openai = new OpenAI()
+const openai = new OpenAI();
 
 export async function POST(request: Request) {
-	try {
-		const body = await request.json();
-		const gameInfo = await findGameInfo(body.gameId);
-		const numRounds = gameInfo?.rounds;
-		const numPlayers = gameInfo?.numPlayers;
-		const totalQuestions = numRounds! * numPlayers!;
+  try {
+    const body = await request.json();
+    const gameInfo = await findGameInfo(body.gameId);
+    const numRounds = gameInfo?.rounds;
+    const numPlayers = gameInfo?.numPlayers;
+    const totalQuestions = numRounds! * numPlayers!;
 
-		const questionCompletion = await openai.chat.completions.create({
-			model: "gpt-3.5-turbo",
-			messages: [
-				{
-					role: "user",
-					content: `Generate 1 random question that people and ChatGPT would have unique responses to, limited to 150 characters. 
+    const questionCompletion = await openai.chat.completions.create({
+      model: "gpt-4-0125-preview",
+      messages: [
+        {
+          role: "user",
+          content: `Generate 1 random question that people and ChatGPT would have unique responses to, limited to 150 characters. 
         Do not generate a controversial question that ChatGPT would not be able to answer. Here are two examples:
         If you could only eat one food for the rest of your life, what would it be? 
         If you could have dinner with any historical figure, who would it be?`,
-				},
-			],
-			temperature: 1.4,
-			n: totalQuestions,
-			max_tokens: 80,
-		});
+        },
+      ],
+      temperature: 1.4,
+      n: totalQuestions,
+      max_tokens: 80,
+    });
 
-		const choices = questionCompletion.choices;
-		const questions = destructureChoices(choices);
+    const choices = questionCompletion.choices;
+    const questions = destructureChoices(choices);
 
-		return NextResponse.json({ response: questions });
-	} catch (error) {
-		console.error(error);
-		return new NextResponse("Internal Server Error", { status: 500 });
-	}
+    return NextResponse.json({ response: questions });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
 
 async function findGameInfo(gameId: string) {
-	const gameInfo = await prisma.game.findFirst({
-		where: {
-			id: gameId,
-		},
-	});
+  const gameInfo = await prisma.game.findFirst({
+    where: {
+      id: gameId,
+    },
+  });
 
-	return gameInfo;
+  return gameInfo;
 }
 
 function destructureChoices(choices: any) {
-	let questions = [];
-	for (let i = 0; i < choices.length; i++) {
-		questions.push(choices[i].message.content);
-	}
+  let questions = [];
+  for (let i = 0; i < choices.length; i++) {
+    questions.push(choices[i].message.content);
+  }
 
-	return questions;
+  return questions;
 }
