bdbe2a2e5384c84d93b04931ad51042d135ca836
Sean Park <seanpark132@gmail.com>
2024-10-14 13:33:56 -0700

    update openai models and request logic from multiple requests to 1 request

--- src/app/api/generateAIResponses/route.ts
+++ src/app/api/generateAIResponses/route.ts
@@ -6,65 +6,38 @@ export async function POST(request: Request) {
 
   try {
     const body = await request.json();
+    const questions: string[] = body.questions;
 
-    // allSettled ensures errors within are handled by inner try catch block (1 error will not cause error in client - aiResponse will be "Error in generating a response")
-    const generateAll = await Promise.allSettled(
-      body.questions.map(async (question: string) => {
-        try {
-          const response = await generateResponse(question);
-          return response;
-        } catch (error) {
-          console.error(error);
-          return "Error in generating a response";
-        }
-      })
-    );
-
-    const allResponses = generateAll.map((obj: any) => {
-      if (obj.value) {
-        return obj.value;
-      } else {
-        return obj.reason;
-      }
+    const responseCompletion = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+        {
+          role: "user",
+          content: `For each of the questions in this array, generate a response that a normal person might respond with: ${questions}. 
+          Put each response into an array of strings and make sure that the responses are placed in the array in the same order as the corresponding questions. In the response ONLY return the array, no other text.
+          Please limit each response to a maximum of 100 characters.`,
+        },
+      ],
+      temperature: 1.5,
+      max_tokens: 80 * questions.length,
     });
 
-    return NextResponse.json({ response: allResponses });
+    const responseContent = responseCompletion.choices[0].message.content;
+    console.log(responseContent);
+    const responsesArray = JSON.parse(
+      responseContent!.replace(/```json\s*|\s*```/g, "")
+    );
+    console.log(responsesArray);
+    if (
+      !Array.isArray(responsesArray) ||
+      !responsesArray.every((q: string) => typeof q === "string")
+    ) {
+      throw new Error("Response content is not a valid array of strings");
+    }
+
+    return NextResponse.json({ response: responsesArray });
   } catch (error) {
     console.error(error);
     return new NextResponse("Internal server error", { status: 500 });
-  }
-
-  async function generateResponse(question: string) {
-    return new Promise((resolve, reject) => {
-      const timeout = setTimeout(() => {
-        clearTimeout(timeout);
-        reject("Error in generating a response");
-      }, 9000);
-
-      openai.chat.completions
-        .create({
-          model: "gpt-4-0125-preview",
-          messages: [
-            {
-              role: "user",
-              content: `Generate 1 normal response to the following question using simple words: "${question}". Please limit the response to a maximum of 100 characters.`,
-            },
-          ],
-          temperature: 1.5,
-          n: 1,
-          max_tokens: 80,
-        })
-        .then((responseCompletion) => {
-          clearTimeout(timeout);
-          const choices = responseCompletion.choices;
-          const response = choices[0].message?.content;
-          const aiResponse = response?.replace(/"/g, "");
-          resolve(aiResponse);
-        })
-        .catch((error) => {
-          clearTimeout(timeout);
-          reject(error);
-        });
-    });
   }
 }
--- src/app/api/generateQuestions/route.ts
+++ src/app/api/generateQuestions/route.ts
@@ -13,24 +13,34 @@ export async function POST(request: Request) {
     const totalQuestions = numRounds! * numPlayers!;
 
     const questionCompletion = await openai.chat.completions.create({
-      model: "gpt-4-0125-preview",
+      model: "gpt-4o",
       messages: [
         {
           role: "user",
-          content: `Generate 1 random question that people and ChatGPT would have unique responses to, limited to 150 characters. 
-        Do not generate a controversial question that ChatGPT would not be able to answer. Here are two examples:
+          content: `Generate ${totalQuestions} random questions that people and ChatGPT would have unique responses to, limited to 150 characters each. Put each question as a string in an array. In the response ONLY return the array, no other text. 
+        Do not generate a controversial question that ChatGPT would not be able to answer. Make sure that each question is different. Here are two examples:
         If you could only eat one food for the rest of your life, what would it be? 
         If you could have dinner with any historical figure, who would it be?`,
         },
       ],
       temperature: 1.4,
-      n: totalQuestions,
-      max_tokens: 80,
+      max_tokens: 80 * totalQuestions,
     });
 
-    const choices = questionCompletion.choices;
-    const questions = destructureChoices(choices);
+    const questions = JSON.parse(
+      questionCompletion.choices[0].message.content!.replace(
+        /```json\s*|\s*```/g,
+        ""
+      )
+    );
+    console.log(questions);
 
+    if (
+      !Array.isArray(questions) ||
+      !questions.every((q: string) => typeof q === "string")
+    ) {
+      throw new Error("Response content is not a valid array of strings");
+    }
     return NextResponse.json({ response: questions });
   } catch (error) {
     console.error(error);
@@ -46,13 +56,4 @@ async function findGameInfo(gameId: string) {
   });
 
   return gameInfo;
-}
-
-function destructureChoices(choices: any) {
-  let questions = [];
-  for (let i = 0; i < choices.length; i++) {
-    questions.push(choices[i].message.content);
-  }
-
-  return questions;
 }
